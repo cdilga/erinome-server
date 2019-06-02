ef2880f2d25d599990af5b18a7996cac567a0cd8
Oliver Coad <oliver.coad@gmail.com>
2019-06-02 20:16:03 +1000

    Add python to deployment

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -49,14 +49,16 @@ let doDeployment token = job {
   "public": true,
   "version": 2,
   "files": [
-    { "file": "index.html", "data": "<!doctype html>\n<html>\n  <head>\n    <title>A litma.af deployment with the Now API!</title>\n    <link rel=\"stylesheet\" href=\"style.css\"> \n </head>\n  <body>\n    <h1>Welcome to a simple static file</h1>\n    <p>Deployed with <a href=\"https://zeit.co/docs/api\">ZEIT&apos;s Now API</a>!</p>\n    <p>This deployment includes three files. A static index.html file as the homepage, a static style.css file for styling, and a date.js serverless function that returns the date on invocation. <img src=\"https://api.checkface.ml/api/erinome\"/> Try <a href=\"/date.js\">getting the date here.</a></p> \n   </body>\n</html>" },
+    { "file": "index.html", "data": "<!doctype html>\n<html>\n  <head>\n    <title>A litma.af deployment with the Now API!</title>\n    <link rel=\"stylesheet\" href=\"style.css\"> \n </head>\n  <body>\n    <h1>Welcome to a simple static file</h1>\n    <p>Deployed with <a href=\"https://zeit.co/docs/api\">ZEIT&apos;s Now API</a>!</p>\n    <p>This deployment includes three files. A static index.html file as the homepage, a static style.css file for styling, and a date.js and date.py serverless function that returns the date on invocation. <img src=\"https://api.checkface.ml/api/erinome\"/> Try <a href=\"/date.js\">getting the date here (node).</a> <a href=\"/date.py\">Or here (python).</a></p> \n   </body>\n</html>" },
     { "file": "style.css", "data": "h1 {\n margin-top: 70px; \n text-align: center; \n font-size: 45px; \n} \n h1, p {\n font-family: Helvetica; \n} \n a {\n color: #0076FF; \n text-decoration: none; \n} \n p {\n text-align: center; \n font-size: 30px; \n} \n p:nth-child(3) { \n font-size: 25px; \n margin-left: 15%; \n margin-right: 15%; \n}" },
+    { "file": "date.py", "data": "import datetime\nfrom http.server import BaseHTTPRequestHandler\n\nclass handler(BaseHTTPRequestHandler):\n\n    def do_GET(self):\n        self.send_response(200)\n        self.send_header('Content-type','text/plain')\n        self.end_headers()\n        self.wfile.write(str(datetime.datetime.now()).encode())\n        return" },
     { "file": "date.js", "data": "module.exports = (req, res) => {\n  res.end(`The time is ${new Date()}`)\n}" }
   ],
   "builds": [
     { "src": "*.js", "use": "@now/node" },
     { "src": "*.html", "use": "@now/static" },
-    { "src": "*.css", "use": "@now/static" }
+    { "src": "*.css", "use": "@now/static" },
+    { "src": "*.py", "use": "@now/python" }
   ]
 }"""        )
         |> getResponse
